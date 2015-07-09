b5bf883834c479e680eb3ae9ac5a2976a02eaae0
Hal Lesesne <hlesesne@samaritan.org>
2015-07-09 00:00:18 -0400

    Updated build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 var gulp = require('gulp');
 var less = require("gulp-less");
+var coffee = require('gulp-coffee');
 var path = require("path");
-gulp.task("default");
-gulp.task("less", function() {
-return gulp.src('./less/**/*.less')
+
+gulp.task("default", buildTask);
+gulp.task("build", buildTask);
+gulp.task("less", lessTask);
+gulp.task('coffee', coffeeTask);
+
+function lessTask() {
+ gulp.src('./less/**/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
     .pipe(gulp.dest('./css'));
-
-});
+}
+function coffeeTask() {
+    gulp.src('./coffee/**/*.coffee')
+    .pipe(coffee({bare: true}))
+    .pipe(gulp.dest('./js'));
+}
+function buildTask() {
+  lessTask();
+  coffeeTask();
+}
